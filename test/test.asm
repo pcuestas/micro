41bc3d9cb87ba3f471a5fe37a64a27e7f57e264e
Pablo Cuesta <pablo.cuestas@estudiante.uam.es>
2022-03-25 12:11:39 +0100

    test

--- test/test.asm
+++ test/test.asm
@@ -3,18 +3,17 @@
 ;**************************************************************************
 ; DATA SEGMENT DEFINITION
 DATA SEGMENT
-    A DB -120,0,1,3,0,0,5,1,1
-    B DB 1,0,1,1,2,1,1,1,0
-    C DB 9 dup(?)
-org 100h
-    STRING DB 100 dup(?)
-    aux1 DB "        $"
-    auxA DB "  |A|=  $"
-    auxB DB "  |B|=  $"
-    auxC DB "  |C|=  $"
-    oper DB "(C)=(A)-(B)$"
-    SEP  DB "  $"
-    TEN  DB 10
+    A DB -20,0,1,3,0,0,5,1,1
+org 20h
+    num DB ?
+org 100h 
+    string DB 20 dup(?)
+    request DB "Enter number: $"
+    ten db 10
+    aux2 db 20 dup(?)
+    aux1 db "      $"
+    auxA db " |A|= $"
+    sep db "  $"
     
 DATA ENDS
 ;**************************************************************************
@@ -42,148 +41,185 @@ BEGIN PROC
     MOV ES, AX
     MOV SP, 100h ; LOAD A STACK POINTER WITH THE HIGHEST VALUE
     ; END OF INITIALIZATIONS
-    ; BEGINNING OF THE PROGRAMME
+    ; BEGINNING OF THE 
     
     mov bx, offset A
-    mov di, offset auxA
-    call print_matrix
+    call print_mat 
+    call print_crlf
+    
+    mov dx, offset request 
+    mov ah, 9 
+    int 21h 
+    
+    mov ah, 0Ah 
+    mov dx, offset string 
+    mov string, 20 ; max length
+    int 21h 
+    call print_crlf 
+     
+    call readnum  ;ax contains the number 
+    
+    mov si, 0 
+    mov bx, ax 
+    bucle:
+        mov al, A[si] 
+        call extend_al 
+        mul bx 
+        mov A[si], al 
+        inc si
+        cmp si, 9 
+        jne bucle 
+
+        
+    mov bx, offset A
+    call print_mat 
     call print_crlf
 
-    mov bx, offset B 
-    mov di, offset auxB
-    call print_matrix 
-    call print_crlf 
-    
-    mov dx, offset oper 
-    call print_string 
-    call print_crlf
-    
-    call calculate_C
-    
-    mov bx, offset C
-    mov di, offset auxC
-    call print_matrix
     ; END OF THE PROGRAMME
     MOV AX, 4C00H
     INT 21H
 BEGIN ENDP
 
-calculate_C proc 
-    mov si, 9
-    calc_c: 
-        mov al, A[si-1]
-        sub al, B[si-1]
-        mov C[si-1], al
-        dec si 
-        jnz calc_c
+extend_al proc 
+    mov ah, 0
+    cmp al, 0
+    jge extend 
+    mov ah, 0FFh 
+    extend:
     ret 
-calculate_C endp 
+extend_al endp 
 
-; prints $ ended string pointed by dx
-print_string proc 
-    push ax
-    mov ah, 9h 
-    int 21h  
-    pop ax 
-    ret
-print_string endp 
+print_mat proc 
+    mov dx, offset aux1 
+    mov ah, 9 
+    int 21h 
+    
+    call print_row 
+    call print_crlf 
+    add bx, 3 
 
-print_crlf proc 
-    push ax 
+    mov dx, offset auxA 
+    mov ah, 9 
+    int 21h 
+    
+    call print_row 
+    call print_crlf 
+    add bx, 3 
+    
+    mov dx, offset aux1 
+    mov ah, 9 
+    int 21h 
+    
+    call print_row 
+    call print_crlf    
+    
+    ret 
+print_mat endp 
+
+al_ascii proc
+    mov aux2[0], ' '
+    mov aux2[1], ' '
+    mov aux2[2], ' '
+    mov aux2[4], '$'
+    mov si, offset aux2[3]
+    mov cx, 0 
+    cmp al, 0 
+    jge ascii_conv 
+        neg al 
+        mov cx, 1 
+    ascii_conv:
+        mov ah, 0 
+        div ten 
+        add ah, '0' 
+        mov [si], ah 
+        dec si 
+        cmp al, 0 
+        jne ascii_conv 
+    jcxz endascii 
+        mov byte ptr [si], '-'
+    endascii: 
+        mov ax, offset aux2
+    ret 
+al_ascii endp
+
+print_al proc
     push dx 
-
-    mov ah, 2h
-    mov dl, 13
-    int 21h 
-    mov dl, 10 
-    int 21h 
-
-    pop dx
-    pop ax 
-    ret
-print_crlf endp 
-
-print_matrix proc 
-    mov dx, offset aux1 
-    call print_string
     
-    call print_row      
-    call print_crlf 
-    add bx, 3 
-     
-    mov dx, di
-    call print_string 
-    call print_row       
-    call print_crlf  
-    add bx, 3 
-    
-    mov dx, offset aux1 
-    call print_string
-     
-    call print_row      
-    call print_crlf 
-    ret
-print_matrix endp 
+    call al_ascii 
+    mov dx, ax 
+    mov ah, 9 
+    int 21h
+
+    pop dx 
+    ret 
+print_al endp
 
 print_row proc 
-    push bx
-
-    mov ah, 2h 
-    mov dl, '|' 
-    int 21h 
-     
-    mov al, [bx] 
-    inc bx 
-    call print_al
-    mov dx, offset SEP 
-    call print_string     
     
-    mov al, [bx] 
-    inc bx 
-    call print_al
-    mov dx, offset SEP 
-    call print_string   
-
-    mov al, [bx] 
-    inc bx 
-    call print_al
-
-    mov ah, 2h 
+    mov ah, 2
     mov dl, '|' 
     int 21h 
 
-    pop bx
+    mov al, [bx]
+    call print_al 
+    mov dx, offset sep 
+    mov ah, 9 
+    int 21h
+    mov al, [bx+1]
+    call print_al 
+    mov dx, offset sep 
+    mov ah, 9 
+    int 21h
+    mov al, [bx+2]
+    call print_al
+    
+    mov ah, 2
+    mov dl, '|' 
+    int 21h 
     ret 
 print_row endp 
 
-print_al proc 
-    mov si, offset STRING[3]
-    mov STRING[0],' '
-    mov STRING[1],' '
-    mov STRING[2],' '
-    mov STRING[3],' '
-    mov STRING[4],'$'
-    mov cx, 0
-    cmp al, 0 
-    jge cont 
-        mov cx, 1
-        neg al
-    cont:
-        mov ah, 0
-        div TEN ; AH=AX%10, AL=AX/10
-        add ah, '0'
-        mov [si], ah
-        dec si
-        cmp al, 0 
-        jne cont 
-    cmp cx,0 
-    je fin 
-        mov BYTE PTR [si], '-'
-    fin: 
-    mov dx, offset STRING 
-    call print_string
-    ret  
-print_al endp 
+readnum proc
+    mov bl, string[1]
+    mov bh, 0
+    mov string[bx+2], ' ' ; end of string 
+    mov bx, offset string[2]
+    mov ax, 0
+    mov cx, 0    
+    cmp byte ptr [bx], '-'
+    jne readn 
+        inc bx
+        mov cx, 1 
+    readn:
+        mov ch, [bx]
+        sub ch, '0' 
+        mul ten 
+        add al, ch
+        inc bx 
+        cmp byte ptr [bx], ' '
+        jne readn 
+    mov ch, 0
+    jcxz readend 
+        neg ax 
+    readend:    
+    ret 
+readnum endp 
+
+print_crlf proc 
+    push ax 
+    push dx
+    mov ah, 2
+    mov dl, 13 
+    int 21h 
+    mov dl, 10 
+    int 21h
+
+    pop dx 
+    pop ax 
+    ret 
+print_crlf endp 
+
+
 
 ; END OF THE CODE SEGMENT
 CODE ENDS
